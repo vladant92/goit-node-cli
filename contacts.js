e5cdd7c906298a44ed60a53e6153017148a54f80
vladant92 <133158157+vladant92@users.noreply.github.com>
2024-04-29 13:15:42 +0300

    complete nodejs hw-01
    complete nodejs hw-01

---
+++ contacts.js
@@ -0,0 +1,42 @@
+import fs from "fs/promises";
+import path from "path";
+import { nanoid } from "nanoid";
+
+const contactsPath = path.resolve("db", "contacts.json");
+const updateContacts = (contacts) =>
+  fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+
+export const listContacts = async () => {
+  const result = await fs.readFile(contactsPath);
+  return JSON.parse(result);
+};
+
+export const getContactById = async (contactId) => {
+  const contacts = await listContacts();
+  const result = contacts.find((contact) => contact.id === contactId);
+  return result || null;
+};
+
+export const addContact = async ({ name, email, phone }) => {
+  const contacts = await listContacts();
+  const newContact = {
+    id: nanoid(),
+    name,
+    email,
+    phone,
+  };
+  contacts.push(newContact);
+  await updateContacts(contacts);
+  return newContact;
+};
+
+export const removeContact = async (contactId) => {
+  const contacts = await listContacts();
+  const index = contacts.findIndex((contact) => contact.id === contactId);
+  if (index === -1) {
+    return null;
+  }
+  const [result] = contacts.splice(index, 1);
+  await updateContacts(contacts);
+  return result;
+};
---
+++ index.js
@@ -0,0 +1,47 @@
+import { program } from "commander";
+
+import * as contactService from "./contacts.js";
+
+program
+  .option("-a, --action <type>", "choose action")
+  .option("-i, --id <type>", "user id")
+  .option("-n, --name <type>", "user name")
+  .option("-e, --email <type>", "user email")
+  .option("-p, --phone <type>", "user phone");
+
+program.parse();
+
+const options = program.opts();
+
+async function invokeAction({ action, id, name, email, phone }) {
+  switch (action) {
+    case "list":
+      const allContacts = await contactService.listContacts();
+      console.table(allContacts);
+      break;
+
+    case "get":
+      const oneContact = await contactService.getContactById(id);
+      console.log(oneContact);
+      break;
+
+    case "add":
+      const newContact = await contactService.addContact({
+        name,
+        email,
+        phone,
+      });
+      console.log(newContact);
+      break;
+
+    case "remove":
+      const removeContact = await contactService.removeContact(id);
+      console.log(removeContact);
+      break;
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+}
+
+invokeAction(options);
